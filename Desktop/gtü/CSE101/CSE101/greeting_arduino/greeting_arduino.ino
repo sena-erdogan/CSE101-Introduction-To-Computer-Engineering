428377eb86683137f21d316b6b158734e9dacff7
Sena Erdoğan <senaerdogan2001@gmail.com>
2022-02-24 13:13:46 +0300

    hw

---
+++ Desktop/gtü/CSE101/CSE101/greeting_arduino/greeting_arduino.ino
@@ -0,0 +1,21 @@
+void setup() {
+ Serial.begin(9600);
+
+}
+
+void loop() {
+  Serial.print(" hi ");
+
+  delay(1000);
+
+  Serial.print(" my name is ");
+
+  delay(1000);
+
+  Serial.print(" Sena Erdoğan ");
+
+  delay(1000);
+
+
+  
+}
